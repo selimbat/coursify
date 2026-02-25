efab394fcc2dbbc730e45d10b29e7d07671cfd21
Selim Tirellil <selim.tirellil@hotmail.fr>
2026-02-25 10:59:41 +0100

    try to fix auth reset on pwa

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -20,7 +20,7 @@ export const actions: Actions = {
         cookies.set(AUTH_COOKIE_NAME, token, {
             path: '/',
             httpOnly: true,
-            sameSite: 'strict',
+            sameSite: 'lax',
             secure: process.env.NODE_ENV === 'production',
             maxAge: 60 * 60 * 24 * 30 // 30 days
         });
